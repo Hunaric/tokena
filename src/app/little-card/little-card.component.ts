e14e4f0d41ff96c1e0dae8a74dbcd66ef68a9ca3
Hunaric <nathanaelfangnon@gmail.com>
2024-09-01 02:15:21 +0100

    Create Little Card Component to load all token

---
+++ src/app/little-card/little-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-little-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './little-card.component.html',
+  styleUrl: './little-card.component.css'
+})
+export class LittleCardComponent {
+
+}
